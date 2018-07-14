e31fbc009622bf9da020e91a34a227054f4726a3
dargaCode <darga.code@gmail.com>
2018-07-13 18:25:24 -0700

    Add basis component structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+class AutocompleteSearch extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: 'searchText',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      searchText: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>AutocompleteSearch</h1>
+        <SearchBox
+          placeholder={this.props.searchPlaceholder}
+          searchText={this.state.searchText}
+          onChange={this.handleChange}
+        />
+        <SuggestionDropdown
+          searchText={this.state.searchText}
+        />
+      </div>
+    );
+  }
+}
+
+class SearchBox extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>SearchBox</h2>
+        <input
+          type="text"
+          placeholder={this.props.placeholder}
+          value={this.props.searchText}
+          onChange={this.props.onChange}
+        />
+      </div>
+    );
+  }
+}
+
+class SuggestionDropdown extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>SuggestionDropdown</h2>
+        <SuggestionItem searchText={this.props.searchText}/>
+        <SuggestionItem searchText={this.props.searchText}/>
+        <SuggestionItem searchText={this.props.searchText}/>
+      </div>
+    );
+  }
+}
+
+class SuggestionItem extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>SuggestionItem - {this.props.searchText}</h3>
+      </div>
+    );
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AutocompleteSearch
+          searchPlaceholder="Searching for financial products"
+        />
       </div>
     );
   }
